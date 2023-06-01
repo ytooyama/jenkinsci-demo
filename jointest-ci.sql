fd4f2815712792f324642ce6b8632ef490888b2f
Youhei Tooyama <ytooyama@users.noreply.github.com>
2023-06-01 17:53:15 +0900

    set ON_ERROR_STOP true

--- jointest-ci.sql
+++ jointest-ci.sql
@@ -1,1 +1,2 @@
+\set ON_ERROR_STOP true
 explain analyze SELECT count(*) FROM t_test AS a, t_join AS b WHERE a.id = b.id GROUP BY a.ten;
--- jointest-ci2.sql
+++ jointest-ci2.sql
@@ -1,3 +1,4 @@
+\set ON_ERROR_STOP true
 explain analyze SELECT count(*)
     FROM    t_test AS a, t_join AS b, t_test AS c,
         t_join AS d, t_test AS e, t_join AS f
